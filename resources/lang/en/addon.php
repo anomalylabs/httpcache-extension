b274036731a03f3897272aa92a1729d949c28200
Ryan Thompson <ryan@pyrocms.com>
2019-11-16 16:48:49 -0600

    1.0

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Httpcache',
+    'name'        => 'Httpcache Extension',
+    'description' => ''
+];
---
+++ tests/Feature/HttpcacheExtensionTest.php
@@ -0,0 +1,10 @@
+<?php namespace Anomaly\HttpcacheExtension\Test\Feature;
+
+class HttpcacheExtensionTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
